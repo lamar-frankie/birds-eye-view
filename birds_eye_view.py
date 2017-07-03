a05b2504ac369f7cd11e4aedadc288a2fabd20ef
Lamar, Frank <frankie_lamar1@homedepot.com>
2017-07-03 11:54:48 -0400

    added basic logic to analyze tweets

--- birds_eye_view.py
+++ birds_eye_view.py
@@ -1,2 +1,22 @@
 import tweepy
-from textblob import textblob
+from textblob import TextBlob
+import credentials
+
+consumer_key = credentials.consumer_key
+consumer_secret = credentials.consumer_secret
+
+access_token = credentials.access_token
+access_token_secret = credentials.access_token_secret
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+
+public_tweets = api.search('Trump')
+
+for tweet in public_tweets:
+    print(tweet.text)
+    analysis = TextBlob(tweet.text)
+    print(analysis.sentiment)
+
