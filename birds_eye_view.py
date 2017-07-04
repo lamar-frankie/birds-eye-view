4108b80ef04f317fb20d6d0dfeb0af0504813a30
Lamar, Frank <frankie_lamar1@homedepot.com>
2017-07-04 10:27:51 -0400

    changed script to return sentiment for 1000 tweets

--- birds_eye_view.py
+++ birds_eye_view.py
@@ -15,7 +15,7 @@ auth.set_access_token(access_token, access_token_secret)
 key_word = input("Enter keyword to search: ")
 api = tweepy.API(auth)
 
-public_tweets = api.search(key_word)
+public_tweets = api.search(q=key_word, count=1000)
 
 for tweet in public_tweets:
     print(tweet.text)
