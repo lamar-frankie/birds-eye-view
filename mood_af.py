41e2873761b1a555861b1999626b13ee9b262392
lamar-frankie <lamar.frankie@gmail.com>
2017-07-19 07:22:36 -0400

    refactored code and removed extra print statements

--- mood_af.py
+++ mood_af.py
@@ -19,55 +19,53 @@ US_WOEID = 23424977
 EXCLUDED_SUBJECTIVITY = 0.0
 EXCLUDED_POLARITY = 0.0
 
+
+
+
+
+def calculate_mood(key_word):
+    mood = 0
+    sentiment_dict = {}
+    public_tweets = api.search(q=key_word, count=10)
+
+    for tweet in public_tweets:
+         analysis = TextBlob(tweet.text)
+
+         if analysis.polarity != EXCLUDED_POLARITY and analysis.subjectivity != EXCLUDED_SUBJECTIVITY:
+             sentiment_dict[tweet.text] = analysis.polarity
+
+    for entry in sentiment_dict:
+        mood += float(sentiment_dict[entry])
+
+    print("Total Mood: " + str(mood))
+    print("Number of entries: " + str(len(sentiment_dict)))
+
+    print("Average Sentiment = " + str(mood/len(sentiment_dict)))
+    return mood
+
+def classify_mood(mood):
+    if mood <= -0.75:
+        print("Miserable")
+
+    elif mood <= -0.50 and mood > -0.75:
+        print("Somber")
+
+    elif mood <= 0.25 and mood > -0.50:
+        print("Disappointing")
+
+    elif mood <= 0.25 and mood > -0.25:
+        print("Meh")
+
+    elif mood <= 0.50 and mood > 25:
+        print("Pleased")
+
+    elif mood <= 0.75 and mood > 0.50:
+        print("Good")
+    elif mood > 0.75:
+        print("Overjoyed")
+
+
 key_word = input("Enter keyword to search: ")
-
-public_tweets = api.search(q=key_word, count=10)
-
-sentiment_dict = {}
-
-#TODO: Fix logic that averages the moods
-for tweet in public_tweets:
-     print(tweet.text)
-     analysis = TextBlob(tweet.text)
-
-     if analysis.polarity != EXCLUDED_POLARITY and analysis.subjectivity != EXCLUDED_SUBJECTIVITY:
-         sentiment_dict[tweet.text] = analysis.polarity
-         print(sentiment_dict)
-         print(len(sentiment_dict))
-
-mood = 0
-
-for entry in sentiment_dict:
-    mood += float(sentiment_dict[entry])
-    print(sentiment_dict.values())
-
-
-
-print("Mood: " + str(mood))
-print("Number of entries: " + str(len(sentiment_dict)))
-
-print("Average Sentiment = " + str(mood/len(sentiment_dict)))
-
-if mood <= -0.75:
-    print("Miserable")
-
-elif mood <= -0.50 and mood > -0.75:
-    print("Somber")
-
-elif mood <= 0.25 and mood > -0.50:
-    print("Disappointing")
-
-elif mood <= 0.25 and mood > -0.25:
-    print("Meh")
-
-elif mood <= 0.50 and mood > 25:
-    print("Pleased")
-
-elif mood <= 0.75 and mood > 0.50:
-    print("Good")
-elif mood > 0.75:
-    print("Overjoyed")
-
-
-
+current_mood = calculate_mood(key_word)
+classify_mood(current_mood)
 
