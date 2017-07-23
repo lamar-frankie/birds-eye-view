f3d2c4b5b6544c5821b3342191a9291b3782e8fc
Lamar, Frank <frankie_lamar1@homedepot.com>
2017-07-22 22:19:58 -0400

    added flask front-end

---
+++ main.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+import datetime
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return '<h1>Mood.af</h1>'
+
+@app.route('/status')
+def status():
+    time_stamp = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    return "Mood.af lives " + time_stamp
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
